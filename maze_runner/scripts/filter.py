6a7f443553a31c4592106d6eb3715c79e892248f
JadeNotJaded <jadeng66@gmail.com>
2019-10-01 18:13:32 -0400

    got p of PID working - working on the rest tonight

--- maze_runner/scripts/filter.py
+++ maze_runner/scripts/filter.py
@@ -7,31 +7,50 @@ from geometry_msgs.msg import Twist
 
 dist_ahead = 0.0
 dist_diag = 0.0
-dist_ahead_threshold = 0.15
-dist_diag_threshold = 0.4
+dist_ahead_threshold = 0.2
+dist_diag_threshold = 0.3
+Kp = 0.03
+Kd = 0.1
+Ki = 0.1
+derivative = 0 #might include later
+integral = 0
 
 def update_dist_ahead(msg):
     global dist_ahead 
     dist_ahead = msg.data
 
 def update_dist_diag(msg):
-    global dist_diag 
-    dist_diag = msg.ranges[int(0.5 / msg.angle_increment)]
+    global dist_diag, integral
+    dist_diag = msg.ranges[int(0.25 / msg.angle_increment)]
+    integral = integral + dist_diag
+
+def pid():
+    global dist_diag_threshold, dist_diag
+    error = abs(dist_diag_threshold - dist_diag)
+    global Kp, Kd, integral, Ki
+    P_value = Kp * error
+    # D_value = Kd * (error - Derivator)
+    # Derivator = error
+    integral = integral + error
+    I_value = integral * Ki
+    return P_value
+
 
 rospy.init_node('filter')
 dist_sub = rospy.Subscriber('distance', Float32, update_dist_ahead)
 scan_sub = rospy.Subscriber('scan', LaserScan, update_dist_diag)
 cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-rate = rospy.Rate(10)
+rate = rospy.Rate(10) #can change this later
 
 while not rospy.is_shutdown():
     vel = Twist()
     if dist_ahead < dist_ahead_threshold:
         vel.angular.z = 1.0
+        vel.linear.x = pid()
     else:
-        vel.linear.x = 0.2
+        vel.linear.x = 0.1
         if dist_diag > dist_diag_threshold:
-            vel.angular.z = -1.0
+            vel.angular.z = -0.5
 
     print("dist_ahead = ", dist_ahead)
     print("dist_diag = ", dist_diag)
